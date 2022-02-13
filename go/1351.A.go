3586f28a76b4127bcebd0a6822cd1ae22182cf20
Aleksandr Kulikov <unnotigkeit@yandex.ru>
2022-02-13 23:42:03 +0300

    Create 1351.A.go

---
+++ go/1351.A.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		var a int
+		var b int
+		fmt.Scan(&a, &b)
+		fmt.Println(a + b)
+	}
+
+}
